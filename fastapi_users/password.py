423fe6a2a622e29aa61d9c7bcb61dcd0bcf09a46
Andrew Chen Wang <acwangpython@gmail.com>
2023-12-25 14:10:58 -0500

    Recreated password helper to use Argon 2

--- fastapi_users/password.py
+++ fastapi_users/password.py
@@ -1,7 +1,8 @@
-from typing import Optional, Protocol, Tuple
+import math
+import secrets
+from typing import Protocol, Tuple
 
-from passlib import pwd
-from passlib.context import CryptContext
+from argon2 import PasswordHasher
 
 
 class PasswordHelperProtocol(Protocol):
@@ -17,20 +18,58 @@ class PasswordHelperProtocol(Protocol):
         ...  # pragma: no cover
 
 
+RANDOM_STRING_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+
+def get_random_string(length, allowed_chars=RANDOM_STRING_CHARS):
+    """
+    Return a securely generated random string.
+
+    The bit length of the returned value can be calculated with the formula:
+        log_2(len(allowed_chars)^length)
+
+    For example, with default `allowed_chars` (26+26+10), this gives:
+      * length: 12, bit length =~ 71 bits
+      * length: 22, bit length =~ 131 bits
+    """
+    return "".join(secrets.choice(allowed_chars) for i in range(length))
+
+
+UNUSABLE_PASSWORD_PREFIX = "!"  # This will never be a valid encoded hash
+UNUSABLE_PASSWORD_SUFFIX_LENGTH = (
+    40  # number of random chars to add after UNUSABLE_PASSWORD_PREFIX
+)
+
+
 class PasswordHelper(PasswordHelperProtocol):
-    def __init__(self, context: Optional[CryptContext] = None) -> None:
-        if context is None:
-            self.context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-        else:
-            self.context = context  # pragma: no cover
+    salt_entropy = 128
+
+    def __init__(self, salt: bytes | None = None):
+        self.ph = PasswordHasher()
+        self._salt: bytes | None = salt
 
     def verify_and_update(
         self, plain_password: str, hashed_password: str
     ) -> Tuple[bool, str]:
-        return self.context.verify_and_update(plain_password, hashed_password)
+        self.ph = PasswordHasher()
+        if self.ph.verify(hashed_password, plain_password):
+            return False, self.generate()
+        if self.ph.check_needs_rehash(hashed_password):
+            return True, self.hash(plain_password)
+        return True, hashed_password
+
+    @property
+    def salt(self):
+        if not self._salt:
+            char_count = math.ceil(self.salt_entropy / math.log2(len(RANDOM_STRING_CHARS)))
+            salt = get_random_string(char_count, allowed_chars=RANDOM_STRING_CHARS)
+            self._salt = salt.encode()
+        return self._salt
 
     def hash(self, password: str) -> str:
-        return self.context.hash(password)
+        return self.ph.hash(password, salt=self.salt)
 
     def generate(self) -> str:
-        return pwd.genword()
+        return UNUSABLE_PASSWORD_PREFIX + get_random_string(
+            UNUSABLE_PASSWORD_SUFFIX_LENGTH
+        )
